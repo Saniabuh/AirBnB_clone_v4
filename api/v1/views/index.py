fba5caa99a9effe6bd12d4e29800b96b561ce9e5
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-11-27 20:22:26 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-""" Index """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
+"""index.py to connect to API"""
 from api.v1.views import app_views
-from flask import jsonify
-
+from flask import Flask, Blueprint, jsonify
+from models import 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
